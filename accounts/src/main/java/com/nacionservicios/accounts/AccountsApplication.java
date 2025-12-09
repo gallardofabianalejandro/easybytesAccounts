7e693dc3301bc932ecd86fcd0342ddff0a676f04
fabian gallardo <EX-CFO07@nacionservicios.com.ar>
2025-12-09 02:00:28 -0300

    feat: Update H2 database configuration for in-memory testi

--- accounts/src/main/java/com/nacionservicios/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/nacionservicios/accounts/AccountsApplication.java
@@ -1,11 +1,9 @@
 package com.nacionservicios.accounts;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class AccountsApplication {
 
     public static void main(String[] args) {
